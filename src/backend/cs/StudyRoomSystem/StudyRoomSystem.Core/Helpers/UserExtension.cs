653d8cc3b55b35c69a55de85c3d05419798ad8b3
AFunDog <1489782679@qq.com>
2025-12-24 15:30:55 +0800

    backend/cs 添加信用分低时的预约限制

---
+++ src/backend/cs/StudyRoomSystem/StudyRoomSystem.Core/Helpers/UserExtension.cs
@@ -0,0 +1,16 @@
+﻿using StudyRoomSystem.Core.Structs.Entity;
+
+namespace StudyRoomSystem.Core.Helpers;
+
+public static class UserExtension
+{
+
+    public static TimeSpan GetBookingLimit(this User user)
+    {
+        if (user.Credits < 40)
+            return TimeSpan.Zero;
+        if (user.Credits < 60)
+            return TimeSpan.FromHours(2);
+        return TimeSpan.FromDays(1);
+    }
+}
